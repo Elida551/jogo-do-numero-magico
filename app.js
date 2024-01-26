32e2594d1d85499be9f922bd734f6f4d00eb092f
Élida Gonçalves <144726724+Elida551@users.noreply.github.com>
2024-01-26 18:23:09 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,66 @@
+    let listaDeNumerosSorteados = [];
+    let numeroLimite = 10;
+    let numeroMagico = gerarNumeroAleatorio();
+    let tentativas = 1;
+
+    function exibirTextoNaTela(tag ,texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak (texto, "Brazilian Portuguese Female",{rate: 1.2});
+    }
+
+    function exibirMensagemInicial() {
+    exibirTextoNaTela("h1" , "Jogo do número mágico");
+    exibirTextoNaTela("p" , "Escolha um número entre 1 e 10");
+    }
+
+    exibirMensagemInicial();
+
+    function verificarChute() {
+        let chute = document.querySelector("input").value;
+
+    if (chute == numeroMagico) {
+        exibirTextoNaTela("h1" , "Acertou!");
+        let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+        let mensagemTentativas = `Parabéns  descobriu o número mágico com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela("p" , mensagemTentativas); 
+        document.getElementById("reiniciar").removeAttribute("disabled");
+    } else {
+        if (chute > numeroMagico) {
+            exibirTextoNaTela("p" ,"O numero mágico é menor");
+        } else {
+            exibirTextoNaTela("p" , " O número mágico é maior");
+        }
+        tentativas++;
+        limparCampo();
+        }
+    }
+
+    function  gerarNumeroAleatorio() {
+    let  numeroEscolhido = parseInt(Math.random() * numeroLimite + 1); 
+    let quantidadeDeElementosNaLista =  listaDeNumerosSorteados.length;
+    
+    if (quantidadeDeElementosNaLista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    }
+    if  (listaDeNumerosSorteados.includes(numeroEscolhido)){
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        console.log (listaDeNumerosSorteados)
+        return numeroEscolhido;
+    }
+    }
+
+    function limparCampo() {
+        chute = document.querySelector("input");
+        chute.value = "";
+    }
+
+    function reiniciarJogo() {
+        numeroMagico = gerarNumeroAleatorio();
+        limparCampo();
+        tentativas = 1;
+        exibirMensagemInicial();
+    document.getElementById("reiniciar").setAttribute("disabled", true);
+    }
